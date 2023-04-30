90bca6c587fdb3348bc278dd6ea40c0deac2a8f4
Tinsae-Birhan1 <tinsaebirhan7@gmail.com>
2023-04-30 23:18:13 +0300

    344. Reverse String

--- 344. Reverse String.py
+++ 344. Reverse String.py
@@ -1,14 +1,17 @@
 class Solution:
-    def revString(self, s, left, right):
+    def revers(self, s, left, right):
         if left >= right:
             return 
+        
         s[left], s[right] = s[right], s[left]
-        self.revString(s, left+1, right-1)
+        self.revers(s, left+1, right-1)
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
-        left =0
+        left = 0
         right = len(s) - 1
-        return self.revString(s, left, right)
+        self.revers(s, left, right)
+
+        
         
