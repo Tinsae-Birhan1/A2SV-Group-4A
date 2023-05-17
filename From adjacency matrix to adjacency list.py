801b21cc7ffa1e3276b04525b6d543584c2254ea
Tinsae-Birhan1 <tinsaebirhan7@gmail.com>
2023-05-17 21:28:08 +0300

    From adjacency matrix to adjacency list

---
+++ From adjacency matrix to adjacency list.py
@@ -0,0 +1,10 @@
+n=int(input())
+
+for i in range(n):
+    row = list(map(int, input().split()))
+    arr=[]
+    
+    for j in range(len(row)):
+        if row[j] == 1:
+            arr.append(j+1)
+    print (len(arr), * arr)
